f5950cc6892f596265a354f0f3c6a84ebe693a05
v.sukharev <vadimsukharev1@yandex.ru>
2023-11-08 11:44:10 +0300

    deleted unused classes

--- core-impl/data/src/main/java/vsukharew/multimodule/data/ProfileRepository.kt
+++ core-impl/data/src/main/java/vsukharew/multimodule/data/ProfileRepository.kt
@@ -4,22 +4,10 @@ import vsukharew.multimodule.coreapi.data.ProfileRepo
 import java.util.UUID
 import javax.inject.Inject
 
-class ProfileRepository @Inject constructor(
-    private val excessiveCompilation: ExcessiveCompilation,
-    private val excessiveCompilation2: ExcessiveCompilation2,
-) : ProfileRepo {
+class ProfileRepository @Inject constructor() : ProfileRepo {
     private val id = UUID.randomUUID()
 
     override fun getProfile() {
-        excessiveCompilation.toString()
         id.toString()
     }
-}
-
-class ExcessiveCompilation @Inject constructor() {
-    private var i = 0
-}
-
-class ExcessiveCompilation2 @Inject constructor() {
-    private var i = 0
 }
