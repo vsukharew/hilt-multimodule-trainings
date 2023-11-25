d6759c64f0b9c953f4ff1b1ec37010bf1bcf1650
v.sukharev <vadimsukharev1@yandex.ru>
2023-11-25 20:00:15 +0300

    deleted scope

--- order-impl/src/main/java/vsukharew/multimodule/order/flow/OrderNavigation.kt
+++ order-impl/src/main/java/vsukharew/multimodule/order/flow/OrderNavigation.kt
@@ -3,9 +3,7 @@ package vsukharew.multimodule.order.flow
 import com.github.terrakok.cicerone.Router
 import vsukharew.multimodule.order_api.OrderRouter
 import javax.inject.Inject
-import javax.inject.Singleton
 
-@Singleton
 class OrderNavigation @Inject constructor(
     private val globalRouter: Router,
 ) : OrderRouter {
